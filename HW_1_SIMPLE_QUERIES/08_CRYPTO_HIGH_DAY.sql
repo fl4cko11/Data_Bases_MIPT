a9b433cae957a3af3e2bc5f09c1c8f587df58cf1
fl4cko11 <khomy4kovvv@gmail.com>
2025-02-25 09:58:41 +0300

    done task08

--- HW_1_SIMPLE_QUERIES/08_CRYPTO_HIGH_DAY.sql
+++ HW_1_SIMPLE_QUERIES/08_CRYPTO_HIGH_DAY.sql
@@ -1,3 +1,14 @@
---ЧЕРЕЗ ПОДЗАПРОСЫ:
--- 1) монеты их макс
--- 2) из 1) выбиарем min даты
+SELECT UPPER(c.full_nm) AS full_name, c.dt, c.high_price AS price
+FROM hw_1.coins c
+JOIN ( --формируем таблицу для каждой монеты - макс прайс
+    SELECT full_nm, MAX(high_price) AS price
+    FROM hw_1.coins
+    GROUP BY full_nm
+) m --m ссылка на резуьтат подзапроса
+ON c.full_nm = m.full_nm AND c.high_price = m.price --меняем основную таблицу: пересекаем по результатам подзапроса
+WHERE c.dt = (
+    SELECT MIN(dt)
+    FROM hw_1.coins --смотрим на изначальную таблицу
+    WHERE full_nm = c.full_nm AND high_price = c.high_price --условие, чтобы для каждого имени и макс цены мин дату найти
+)
+ORDER BY c.high_price DESC, c.full_nm;
