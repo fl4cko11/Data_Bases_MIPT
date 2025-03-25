d7784dc02b5515d8dd0b8af6065333f21d46b7f1
fl4cko11 <khomy4kovvv@gmail.com>
2025-03-25 13:50:15 +0300

    corrects2.0

---
+++ Delivery_DB_Project/Queries/02_unpopular_client.sql
@@ -0,0 +1,9 @@
+SELECT name
+FROM (
+    SELECT dp.clients.name AS name, COUNT(dp.h_orders.client_id) AS count
+    FROM dp.h_orders
+    LEFT JOIN dp.clients ON (dp.clients.client_id = dp.h_orders.client_id)
+    GROUP BY dp.h_orders.client_id, dp.clients.name
+    ORDER BY count ASC
+    LIMIT 1
+);
